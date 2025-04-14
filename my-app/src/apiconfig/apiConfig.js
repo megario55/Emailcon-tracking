f1a23902316b3d42a1924f3263d230086d7507d4
megario55 <142385323+megario55@users.noreply.github.com>
2025-04-14 17:33:00 +0530

    Update apiConfig.js 14/4/25 new backend

--- my-app/src/apiconfig/apiConfig.js
+++ my-app/src/apiconfig/apiConfig.js
@@ -1,7 +1,8 @@
 // src/apiConfig.js
 const apiConfig = {
-    baseURL:"https://emailcon-tracking-new.onrender.com",
+    baseURL:"https://emailcon-backend.onrender.com",
     //baseURL:"http://localhost:8000",
 };
 
 export default apiConfig;
+
