8291cb339be861d3430401e319096af9a7b5a6ed
megario55 <142385323+megario55@users.noreply.github.com>
2025-04-15 10:34:48 +0530

    Update Home.jsx 15/4/25

--- my-app/src/component/Home.jsx
+++ my-app/src/component/Home.jsx
@@ -493,7 +493,7 @@ const Home = () => {
                       if (!item || !item.type) return null;
                       return (
                         <div
-                          className="content-item-preview"
+                          className="content-item-preview-home"
                           style={item.style}
                           key={index}
                         >
